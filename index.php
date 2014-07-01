5860b7fa1ce24bae04b2b52a38db472bc76ca247
Jim Mello <jmello@g.harvard.edu>
2014-06-30 22:40:15 -0400

    More style changes, finished adding content.

--- index.php
+++ index.php
@@ -5,20 +5,25 @@
 	<title>Jim Mello - Project 1</title>
 	<style>
 		.leftdiv {
-			float:left;
-		}
-				
-		.rightdiv {
-			float:right;
+			font-family:Tahoma, Geneva, sans-serif;
 			text-align:left;
 		}
-		
-		h1 {
+				
+		.centerdiv {
 			text-align:center;
 		}
 		
+		h1 {
+			font-family:Tahoma, Geneva, sans-serif;
+			font-size:24pt;
+			text-align:center;
+		}
+		
+		h2 {
+			font-size:18pt;
+		}
+		
 		span {
-			font-family:Calibri;
 			font-size:16pt;
 			color:blue;
 		}
@@ -27,37 +32,43 @@
 
 	<body>
 		<h1>Jim Mello - Project 1</h1>
-		<div class="rightdiv">
+		<div class="centerdiv">
 			<img alt="Jim in Switzerland" src="/images/Jim.png" />
 		</div>
 		<div class="leftdiv">
 			<div>
 				<h2>In 2+ sentences describe who you are and what you do.</h2>
-				<span></span>
+				<span>My name is Jim Mello.  I'm a software engineer manager at an enterprise software company headquartered in Cambridge, MA.  I've been working there for 18+ years in a multitude of roles within the Engineering department.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>Why are you taking this course?</h2>
 				<span>I am taking Dynamic Web Applications primarily to learn PHP.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>What previous experience do you have with HTML/CSS?</h2>
-				<span>I have been working with HTML & CSS for about 8 years.</span>
+				<span>I have been working with HTML & CSS for about 10 years.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>What previous experience do you have with programming?</h2>
-				<span>I have been a software engineer for over 15 years, working on C++ and Java-based enterprise applications.</span>
+				<span>I have been a software engineer for over 15 years, working primarily on C++ and Java-based enterprise applications.  I also have some experience with JavaScript and SQL.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>Have you worked with PHP before? If so, to what extent?</h2>
 				<span>I have never worked with PHP before.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>Are you working on a Mac, PC, or some other platform?</h2>
 				<span> I am working on two PCs, both running Windows 7.</span>
 			</div>
+				<br/><br/>
 			<div>
 				<h2>Anything else we should know?</h2>
-				<span></span>
+				<span>I'm very excited to be taking this class.</span>
 			</div>
 		</div>
 	</body>
